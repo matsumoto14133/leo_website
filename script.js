b7f517ba23747b3a032d45c8ba4a56d688891122
matsumoto <matsumoto.14133@gmail.com>
2025-10-16 19:16:17 +0900

    バフシミュレーター追加2

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const navToggle = document.querySelector('.nav-toggle');
 const navList = document.getElementById('nav-list');
 const isHomePage = window.location.pathname.endsWith('index.html') || window.location.pathname === '/';
 
-// 初期状態はメニューを表示
-if (isHomePage) {
-  navList.classList.add('active');  // index.htmlでは表示
-} else {
-  navList.classList.remove('active'); // 他ページでは非表示
-}
-
 // メニューの表示切り替え
 navToggle.addEventListener('click', () => {
   navList.classList.toggle('active');
+});
+
+// ページ読み込み後に実行
+window.addEventListener('DOMContentLoaded', () => {
+  // 初期状態はメニューを表示
+  if (isHomePage) {
+    navList.classList.add('active');  // index.htmlでは表示
+  } else {
+    navList.classList.remove('active'); // 他ページでは非表示
+  }
 });
